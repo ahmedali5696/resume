17f9b44e7d77fdbac4cdda03d87afa858cdfa88d
Ahmed Ali <ahmedali5696@outlook.com>
2021-01-07 02:23:15 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-window.
+const bodyRect = document.body.getBoundingClientRect(),
+    elemRect = document.getElementById('bio').getBoundingClientRect(),
+    offset   = elemRect.top - bodyRect.top;
+
+
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > offset){
+        // document.getElementById('nav').className = 'fixed';
+        document.getElementById('nav').style.position = 'fixed';
+    } else {
+        document.getElementById('nav').style.position = 'absolute';
+        
+    }
+})
