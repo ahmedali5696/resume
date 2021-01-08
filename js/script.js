98554fe6ef891cea1a9fd1ef6e72b5395625bc06
Ahmed Ali <ahmedali5696@outlook.com>
2021-01-08 23:10:57 +0200

    update

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ btn.addEventListener('click', () => {
 
     if (list.classList.contains('collapsed')) {
         list.classList.remove('collapsed');
-    } else  {
+    } else {
         list.classList.add('collapsed');
     }
 })
