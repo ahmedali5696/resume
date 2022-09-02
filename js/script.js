b49144a87cc68c8e378d1c1be5516723df397b37
Ahmed Ali <ahmedali5696@outlook.com>
2022-09-02 22:16:40 +0200

    Update the code and add more things

--- js/script.js
+++ js/script.js
@@ -1,50 +1,80 @@
-/*jshint esversion: 6 */
-document.getElementById('main').setAttribute('href', window.location.origin + '/resume');
 
-const offset = document.getElementById('nav').offsetTop,
-    btn = document.getElementById('collapse');
+const toggleBtn = document.getElementById('navbar__toggler');
+const navbarList = document.querySelector('.navbar__nav');
+const navItem = document.querySelectorAll('.nav-item');
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY > offset) {
-        document.getElementById('nav').className = 'fixed';
-    } else {
-        document.getElementById('nav').classList.remove('fixed');
-    }
-});
 
-// Scrolling Listener
-document.addEventListener('scroll', () => {
-
-    // loop on all nav links to get his atrr
-    document.querySelectorAll('#nav li a').forEach(link => {
-
-        // store current element to use it to compare sections ids
-        const currLink = link,
-            hrefElement = currLink.getAttribute('href');
-
-        // Loop on all sections to get his offsetTop and compare it with currunt link id
-        document.querySelectorAll('body section').forEach(section => {
-            // store section offsetTop
-            const sectionOffset = section.offsetTop,
-                // Store all li at nav to loop on it
-                items = document.querySelectorAll('.item');
-
-            // condition to add active class to right link
-            if (window.scrollY > sectionOffset - 2 && hrefElement.includes(section.id)) {
-                // loop on li to remove any active class before add the new
-                items.forEach(item => {
-                    if (item.classList.contains('active')) {
-                        item.classList.remove('active');
-                    }
-                });
-                // add acive class to section link in nav
-                currLink.parentElement.classList.add('active');
-            }
-        });
-    });
-});
-
-btn.addEventListener('click', () => {
-    const list = document.getElementById('list');
-    list.classList.toggle('collapsed');
+// Navbar Toggler
+toggleBtn.addEventListener('click', () => {
+  const list = document.querySelector('.navbar__nav');
+  list.classList.toggle('collapsed');
 })
+
+
+// Add active class to navbar's item
+navbarList.addEventListener('click', (e) => {
+  navItem.forEach(item => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    }
+  })
+  e.target.parentElement.classList.add('active')
+})
+
+
+// Change navbar active class by scrolling
+// document.addEventListener('scroll', () => {
+
+//   // loop on all nav links to get his atrr
+//   document.querySelectorAll('#nav li a').forEach(link => {
+
+//       // store current element to use it to compare sections ids
+//       const currLink = link,
+//           hrefElement = currLink.getAttribute('href');
+
+//       // Loop on all sections to get his offsetTop and compare it with currunt link id
+//       document.querySelectorAll('body section').forEach(section => {
+//           // store section offsetTop
+//           const sectionOffset = section.offsetTop,
+//               // Store all li at nav to loop on it
+//               items = document.querySelectorAll('.nav-item');
+
+//           // condition to add active class to right link
+//           if (window.scrollY >= sectionOffset - 2 && hrefElement.includes(section.id)) {
+
+//               // loop on li to remove any active class before add the new
+//               items.forEach(item => {
+//                   if (item.classList.contains('active')) {
+//                       item.classList.remove('active');
+//                   }
+//               });
+
+//               // add acive class to section link in nav
+//               currLink.parentElement.classList.add('active');
+//           }
+//       });
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
